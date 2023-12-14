8714401fc374c2959d7f66921f54d6d081ef04eb
nada <dhzmqjhf@gmail.com>
2023-12-14 22:31:51 +0900

    mock data 이동

---
+++ src/mocks/subData.js
@@ -0,0 +1,178 @@
+export const NAV_ITEM = [
+  {
+    id: 1,
+    path: "/backtest/backtest/universe",
+    pathName: "유니버스 선택",
+  },
+  {
+    id: 2,
+    path: "/backtest/backtest/factors",
+    pathName: "팩터 선택",
+  },
+  {
+    id: 3,
+    path: "/backtest/backtest/backtest",
+    pathName: "백테스트 셜졍",
+  },
+];
+
+export const UNIVERS = [
+  {
+    id: 1,
+    name: "한국",
+  },
+  {
+    id: 2,
+    name: "미국",
+  },
+  {
+    id: 3,
+    name: "KOSPI",
+  },
+  {
+    id: 4,
+    name: "KOSDAQ",
+  },
+];
+
+export const INITIAL_FITERS = [
+  {
+    id: 1,
+    name: "금융주 제외",
+  },
+  {
+    id: 2,
+    name: "지주사 제외",
+  },
+  {
+    id: 3,
+    name: "관리종목 제외",
+  },
+  {
+    id: 4,
+    name: "적자기업 제외",
+  },
+  {
+    id: 5,
+    name: "적자기업 제외(년간)",
+  },
+  {
+    id: 6,
+    name: "중국기업 제외",
+  },
+  {
+    id: 7,
+    name: "PTP 기업 제외",
+  },
+  {
+    id: 8,
+    name: "소형주 하위 20%만",
+  },
+];
+
+export const EXCEPT_SECTORS = [
+  {
+    id: 1,
+    name: "건강관리",
+  },
+  {
+    id: 2,
+    name: "자동차",
+  },
+  {
+    id: 3,
+    name: "화장품,의류,완구",
+  },
+  {
+    id: 4,
+    name: "보험",
+  },
+  {
+    id: 5,
+    name: "필수 소비재",
+  },
+  {
+    id: 6,
+    name: "운송",
+  },
+  {
+    id: 7,
+    name: "상사,자본재",
+  },
+  {
+    id: 8,
+    name: "비철,목재 등",
+  },
+  {
+    id: 9,
+    name: "화학",
+  },
+  {
+    id: 10,
+    name: "건설,건축관련",
+  },
+  {
+    id: 11,
+    name: "에너지",
+  },
+  {
+    id: 12,
+    name: "기계",
+  },
+  {
+    id: 13,
+    name: "철강",
+  },
+  {
+    id: 14,
+    name: "반도체",
+  },
+  {
+    id: 15,
+    name: "IT 하드웨어",
+  },
+  {
+    id: 16,
+    name: "통신서비스",
+  },
+  {
+    id: 17,
+    name: "증권",
+  },
+  {
+    id: 18,
+    name: "디스플레이",
+  },
+  {
+    id: 19,
+    name: "IT 가전",
+  },
+  {
+    id: 20,
+    name: "소매(유통)",
+  },
+  {
+    id: 21,
+    name: "유틸리티",
+  },
+  {
+    id: 22,
+    name: "미디어,교육",
+  },
+  {
+    id: 23,
+    name: "은행",
+  },
+  {
+    id: 24,
+    name: "호텔,레저서비스",
+  },
+  {
+    id: 25,
+    name: "소프트웨어",
+  },
+  {
+    id: 26,
+    name: "조선",
+  },
+];
